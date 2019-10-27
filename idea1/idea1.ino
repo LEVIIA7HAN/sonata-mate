770903f3775ad87c28aff34cb5e51653584870d3
Howard C <45508175+LEVIIA7HAN@users.noreply.github.com>
2019-10-26 19:09:12 -0700

    Update idea1.ino

--- idea1/idea1.ino
+++ idea1/idea1.ino
@@ -1,12 +1,18 @@
-#include "pitches.h"
 #include <Wire.h>
 #include "rgb_lcd.h"
 
-const int potentiometer = A0; //assigns potentiometer to pin A0
-const int angle = 300; //signifies the angle that the potentiometer can reach max
+const int pitchKnob = A0; //assigns potentiometer to pin A0; changes pitch/note 300 divided by 7, float value degrees per note, display note on screen
+const int volKnob = A1; //assigns potentiometer to pin A1; changes volume, divide by 3 to get an int, not float between 0-100 for volume values.
+const int octaveKnob = A2; //assigns potentiometer to pin A2; changes octave, 300 degrees divided by 100 to get 0-3 degrees per octave, 0-1 for lower, 1.1 - 2 for mid, 2.1 - 3 for max oct
 const int button = 8; //assigns button to pin 8
 const int buzzer = 4; //assigns buzzer to pin 4
 const int led = 3; //assigns led to pin 3
+
+const int angle = 300; //signifies the angle that the potentiometer can reach max
+
+int pitchVal = 0;
+int volumeVal = 0;
+int octaveVal = 0; //variable to store value of Octave,
 
 //screen rgb
 rgb_lcd lcd;
@@ -23,22 +29,15 @@ byte musicNote[8] = {
     0b01110
 };
 
-//notes array
-int notes[] = {
-  NOTE_B0,NOTE_C1,NOTE_D1,NOTE_E1,NOTE_F1,NOTE_G1,NOTE_A1,
-  NOTE_B1,NOTE_C2,NOTE_D2,NOTE_E2,NOTE_F2,NOTE_G2,NOTE_A2,
-  NOTE_B2,NOTE_C3,NOTE_D3,NOTE_E3,NOTE_F3,NOTE_G3,NOTE_A3
-  };
-
-//sets initial tone to 0? idk what the fuck this does lol
-int Tone = 0;
-int speakerPine = 2;
-
 //assigns input/output values to components
 void setup() {
-  //assigns modes to pins
-  pinMode(potentiometer, INPUT);
+  //ins
+  pinMode(pitchKnob, INPUT);
+  pinMode(volKnob, INPUT);
+  pinMode(octaveKnob, INPUT);
   pinMode(button, INPUT);
+
+  //outs
   pinMode(buzzer, OUTPUT);
   pinMode(led, OUTPUT);
 
@@ -60,28 +59,22 @@ void ignitionButt(){
   delay(1);
   }
 
-void changePitch(){
-  int sensorValue = analogRead(A0); //puts the value of potent onto varaible sensorValue 
-  int pitch = sensorValue/49; //pitch is sensorValue divided by 49 to get 6 pitches?
-  }
-
 void firstDisplay(){
   lcd.setCursor(0,1);//changes the place to print
   lcd.print(millis()/1000); //changeable lcd value
   }
 
-void strobe(unsigned char color){
+void porygon(unsigned char color){
   for (int i = 0; i < 255; i++){
     lcd.setPWM(color, i);
     }
   }
 
 void loop() {
-  ignitionButt();
-  changePitch();
-  firstDisplay();
-  strobe(REG_RED);
-  strobe(REG_GREEN);
-  strobe(REG_BLUE);
+  ignitionButt(); //button for moosic
+  firstDisplay(); //displays static nonmoving text
+  porygon(REG_RED); //EPILEPSPY EPILEPSPY EPILEPSPY EPILEPSPY EPILEPSPY EPILEPSPY EPILEPSPY EPILEPSPY EPILEPSPY EPILEPSPY EPILEPSPY EPILEPSPY EPILEPSPY 
+  porygon(REG_GREEN); //EPILEPSPY EPILEPSPY EPILEPSPY EPILEPSPY EPILEPSPY EPILEPSPY EPILEPSPY EPILEPSPY EPILEPSPY EPILEPSPY EPILEPSPY EPILEPSPY EPILEPSPY 
+  porygon(REG_BLUE); //EPILEPSPY EPILEPSPY EPILEPSPY EPILEPSPY EPILEPSPY EPILEPSPY EPILEPSPY EPILEPSPY EPILEPSPY EPILEPSPY EPILEPSPY EPILEPSPY EPILEPSPY 
   
 }
